1a502fd5c7732fc81f73fb1354596f80934b49e6
Obinna Akuma <akumavincent140@gmail.com>
2024-06-28 22:36:45 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/circularprogressbar/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.circularprogressbar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.circularprogressbar.ui.theme.CircularProgressBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CircularProgressBarTheme {
+                Box(contentAlignment = Alignment.Center
+                    , modifier = Modifier.fillMaxSize())
+                {
+                    CircularProgressBar(percentage = 0.9f, number = 100)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    color: Color = Color.Magenta,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    animDelay: Int = 0,
+    animDuration: Int = 1000
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        ), label = "FloatAnimation"
+    )
+
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    )
+    {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * currentPercentage.value,
+                useCenter = false, //mind you here is false
+                style = Stroke(width = 17f)
+            )
+
+        }
+
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            //fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            fontSize = fontSize
+        )
+    }
+
+}
